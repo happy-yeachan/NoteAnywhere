67597f0cb618d1714cbad5f44e39fe312a29316e
yeachan <dus001228@naver.com>
2025-05-06 17:25:39 +0900

    Spring Boot Start

---
+++ src/main/java/com/dannyPlaygroud/noteAnywhere/NoteAnywhereApplication.java
@@ -0,0 +1,13 @@
+package com.dannyPlaygroud.noteAnywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteAnywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteAnywhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dannyPlaygroud/noteAnywhere/NoteAnywhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyPlaygroud.noteAnywhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAnywhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
